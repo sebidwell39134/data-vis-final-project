0e89e9388d6004f41fec6edf3b02feb2f830ddea
Sam Bidwell <seb260@scarletmail.rutgers.edu>
2024-05-08 23:45:05 -0400

    Add write-up and fine-tune tree module code/readme

--- spanningTreeModule.py
+++ spanningTreeModule.py
@@ -1,5 +1,4 @@
 import igraph as ig
-import numpy as np
 import itertools
 
 def generateSpanningTreeDict(graph):
@@ -20,44 +19,41 @@ def generateSpanningTreeDict(graph):
         index += 1
     return spanningTreeDict
 
-def treeSetToMultipleMetaGraphs(spanningTreeDict, chromaticNumber, graph):
-    #generate a graph, with each tree as a node, and edges between trees if they're "adjacent"
-    #that is to say, if they differ by exactly one edge
-    #This is done by constructing an appropriate dict of lists and using the igraph method to make a graph from that
-    metaGraphDictComplete = dict.fromkeys(spanningTreeDict.keys())
+def treeSetToMetaGraph(spanningTreeDict, chromaticNumber, graph):
+    #Generate a graph, with each tree as a node, and edges between trees if they're "adjacent"
+    #That is to say, if they differ by exactly one edge
+    #Further, the edge is only made if the next tree is "better" at coloring the original graph
     metaGraphDictGreedy = dict.fromkeys(spanningTreeDict.keys())
     errorDict = dict.fromkeys(spanningTreeDict.keys())
     for key in errorDict.keys():
         keyError = incorrectEdges(spanningTreeDict[key], chromaticNumber, graph)
         errorDict[key] = keyError
     for key in spanningTreeDict.keys():
-        metaGraphDictComplete[key] = []
         metaGraphDictGreedy[key] = []
         for iteratedKey in spanningTreeDict.keys():
-            if len(set(spanningTreeDict[key][0]).difference(set(spanningTreeDict[iteratedKey][0]))) == 1: 
-            #this equals 2 if and only if there is exactly one edge in each not in the other
-                metaGraphDictComplete[key].append(iteratedKey)
-                if errorDict[iteratedKey] < errorDict[key]:
-                    metaGraphDictGreedy[key].append(iteratedKey)
-    metaGraphComplete = ig.Graph.ListDict(metaGraphDictComplete, directed=False)
+            if len(set(spanningTreeDict[key][0]).difference(set(spanningTreeDict[iteratedKey][0]))) == 1 and errorDict[iteratedKey] < errorDict[key]: 
+                metaGraphDictGreedy[key].append(iteratedKey)
     metaGraphGreedy = ig.Graph.ListDict(metaGraphDictGreedy, directed=True)
-    return metaGraphComplete, metaGraphGreedy, errorDict
+    return metaGraphGreedy, errorDict
 
 def isSpanningTree(edgeList, vertexCount):
+    #A simple check but nicer as a function
     tempGraph = ig.Graph.TupleList(edgeList)
     return (tempGraph.ecount() == vertexCount - 1 and tempGraph.vcount() == vertexCount and tempGraph.is_connected())
 
 def incorrectEdges(spanningTuple, chromaticNumber, graph):
+    #Apply the flooding algorithm and find the number of edges in the original graph between vertices of the same color
     incorrectEdges = 0
     spanningTreeGraph = spanningTuple[1]
-    vertex = 0  #arbitrarily defined to be the tree's root vertex, but always the same vertex of the underlying graph
+    vertex = 0  #the tree's root vertex, which will always be the same vertex of the underlying graph
     for offset in range(chromaticNumber):
         colorClass = []
-        for factor in range(-(spanningTreeGraph.diameter()//-chromaticNumber)): #I want to ceiling the result here so double-negation is used
-            factorColorClassIndices = spanningTreeGraph.neighborhood(vertex, order = chromaticNumber * factor + offset, mindist = chromaticNumber * factor + offset)
+        for factor in range(spanningTreeGraph.diameter() + 1):
+            distance = chromaticNumber * factor + offset
+            factorColorClassIndices = spanningTreeGraph.neighborhood(vertex, order = distance, mindist = distance)
             factorColorClassNames = [spanningTreeGraph.vs[index]['name'] for index in factorColorClassIndices]
             colorClass = colorClass + factorColorClassNames
         for vertexPair in itertools.combinations(colorClass, 2):
             if vertexPair[0] in graph.neighbors(vertexPair[1]):
-                    incorrectEdges += 1
+                incorrectEdges += 1
     return incorrectEdges
